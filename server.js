df680685acce0c111331b5ce8e6d2289c0b439ec
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2017-06-26 16:00:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/cancer', function(req,res){
 });
 
 app.get('/version', function(req,res){
-    res.send('Hello world v1.1 ' + os.hostname() + '\n');
+    res.send('Hello world v1.2 ' + os.hostname() + '\n');
 });
 
 app.get('/*', function(req, res){
